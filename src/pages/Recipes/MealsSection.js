6f46cdf9b92f0dadca00591ac038b84bc6075899
DalvinderSingh2022 <dalvinderSingh201999@gmail.com>
2024-01-23 20:10:47 +0530

    changes for deployment

--- src/pages/Recipes/MealsSection.js
+++ src/pages/Recipes/MealsSection.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 import MealCard from '../../components/MealCard';
 import useFetch from '../../components/useFetch';
--- src/pages/Recipes/SearchSection.js
+++ src/pages/Recipes/SearchSection.js
@@ -8,26 +8,29 @@ const SearchSection = ({ setSearch, setCategory }) => {
     const [isLoading, isError, { categories }] = useFetch('/categories.php');
     const [value, setValue] = useState('');
 
-    return (
-        <>
-            <section className='filters flex animate Bottom'>
-                <div className="flex full search" >
-                    <input
-                        type="text"
-                        placeholder='Recipe name...'
-                        value={value}
-                        onChange={e => setValue(e.target.value)}
-                    />
-                    {value && <button className='btn flex' onClick={(e) => setSearch(value)}><FaSearch /></button>}
-                </div>
-                <select className='btn' name="categories" id="categories" onChange={e => setCategory(e.target.value)}>
-                    <option value={''}>categories</option>
-                    {categories && categories.map(({ idCategory, strCategory }) =>
-                        <option value={strCategory} key={idCategory}>{strCategory}</option>
-                    )}
-                </select>
-            </section>
-        </>)
+    if (!isLoading && !isError) {
+        return (
+            <>
+                <section className='filters flex animate Bottom'>
+                    <div className="flex full search" >
+                        <input
+                            type="text"
+                            placeholder='Recipe name...'
+                            value={value}
+                            onChange={e => setValue(e.target.value)}
+                        />
+                        {value && <button className='btn flex' onClick={(e) => setSearch(value)}><FaSearch /></button>}
+                    </div>
+                    <select className='btn' name="categories" id="categories" onChange={e => setCategory(e.target.value)}>
+                        <option value={''}>categories</option>
+                        {categories && categories.map(({ idCategory, strCategory }) =>
+                            <option value={strCategory} key={idCategory}>{strCategory}</option>
+                        )}
+                    </select>
+                </section>
+            </>)
+    }
+
 }
 
 export default SearchSection;
